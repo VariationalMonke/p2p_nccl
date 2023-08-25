8a456df2222d69cdeb86172de2e17ce5978a19f6
VariationalMonke <124775888+VariationalMonke@users.noreply.github.com>
2023-08-25 09:23:04 +0100

    changes
    some changes with usage of nccl

--- nccl_bw.cu
+++ nccl_bw.cu
@@ -12,41 +12,48 @@ void checkNcclError(ncclResult_t result, int line) {
 }
 #define NCCL_CHECK(cmd) checkNcclError(cmd, __LINE__)
 
-float P2PBandwidthTest(int device_id1, int device_id2) {
+float P2PBandwidthTest(int device_id1, int device_id2, ncclComm_t *comms, ncclUniqueId id) {
+    // Memory allocation
     float* sendBuffer;
     float* recvBuffer;
-    
+
+    cudaStream_t s1 = (cudaStream_t)malloc(sizeof(cudaStream_t));
+    cudaStream_t s2 = (cudaStream_t)malloc(sizeof(cudaStream_t));
+
+    // Allocate buffer and prepare stream for each gpu
     cudaSetDevice(device_id1);
     cudaMalloc(&sendBuffer, kDataSize * sizeof(float));
     cudaMemset(sendBuffer, 1.0, kDataSize * sizeof(float));
+    cudaStreamCreate(&s1);
 
     cudaSetDevice(device_id2);
     cudaMalloc(&recvBuffer, kDataSize * sizeof(float));
     cudaMemset(recvBuffer, 0.0, kDataSize * sizeof(float));
-
-    ncclComm_t comm;
-    int rank = 0;
-    ncclUniqueId id;
-    NCCL_CHECK(ncclGetUniqueId(&id));
-    NCCL_CHECK(ncclCommInitRank(&comm, 2, id, rank));
-
-    for (int i = 0; i < kWarmUpTurns; ++i) {
-        NCCL_CHECK(ncclSend(sendBuffer, kDataSize, ncclFloat, device_id2, comm, cudaStreamDefault));
-    }
+    cudaStreamCreate(&s2);
 
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
 
+    // Record elapsed time for rank n send and rank m recv
     cudaEventRecord(start);
-    NCCL_CHECK(ncclSend(sendBuffer, kDataSize, ncclFloat, device_id2, comm, cudaStreamDefault));
+    ncclGroupStart();
+    NCCL_CHECK(ncclSend(sendBuffer, kDataSize, ncclFloat, device_id2, comms[device_id1], s1));
+    NCCL_CHECK(ncclRecv(recvBuffer, kDataSize, ncclFloat, device_id1, comms[device_id2], s2));
+    cudaStreamSynchronize(s1);
+    cudaStreamSynchronize(s2);
+    ncclGroupEnd();
     cudaEventRecord(stop);
     cudaEventSynchronize(stop);
 
     float milliseconds = 0;
     cudaEventElapsedTime(&milliseconds, start, stop);
-
-    NCCL_CHECK(ncclCommDestroy(comm));
+ 
+    // Clean up
+    cudaSetDevice(device_id1);
+    NCCL_CHECK(ncclCommDestroy(comms[device_id1]));
+    cudaSetDevice(device_id2);
+    NCCL_CHECK(ncclCommDestroy(comms[device_id2]));
 
     cudaSetDevice(device_id1);
     cudaFree(sendBuffer);
@@ -55,24 +62,38 @@ float P2PBandwidthTest(int device_id1, int device_id2) {
     cudaFree(recvBuffer);
 
     return milliseconds / 1000.0; // Convert to seconds
+    return 0.0;
 }
 
 int main() {
+    int version;
+    ncclGetVersion(&version);
+    printf("nccl version: %d\n", version);
+
     int numGPUs;
     cudaGetDeviceCount(&numGPUs);
+    printf("device count: %d\n", numGPUs);
 
     if (numGPUs < 2) {
         std::cout << "Error: At least two GPUs are required." << std::endl;
         return 0;
     }
 
-    for (int index1 = 0; index1 < numGPUs; ++index1) {
-        for (int index2 = 0; index2 < numGPUs; ++index2) {
-            if (index1 == index2) continue; // Skip self-to-self communication
+    ncclComm_t comms[numGPUs];
+    ncclUniqueId id;
+    NCCL_CHECK(ncclGetUniqueId(&id));
+    // create communicator for each GPU in a single node
+    ncclCommInitAll(comms, numGPUs, NULL);
+    // for(int i = 0; i < numGPUs; i++) {
+    //     cudaSetDevice(i);
+    //     ncclCommInitRank(&(comms[i]), numGPUs, id, i);
+    // }
 
-            float time = P2PBandwidthTest(index1, index2);
+    for (int i = 0; i < numGPUs; i++) {
+        for (int j = 0; j < numGPUs; j++) {
+            float time = P2PBandwidthTest(i, j, comms, id);
             float bandwidth = (kDataSize * sizeof(float) / 1024.0 / 1024.0 / 1024.0) / time; // in GB/s
-            printf("GPU %d -> GPU %d: Bandwidth: %10.2f GB/s\n", index1, index2, bandwidth);
+            printf("GPU %d -> GPU %d: Bandwidth: %10.2f GB/s\n", i, j, bandwidth);
         }
     }
 
